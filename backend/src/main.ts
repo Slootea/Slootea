d768a017b65e046795890cc539db161d610dd21e
AlfaPigeon <oguzkaan.ozsoy@hotmail.com>
2026-01-18 20:51:49 +0300

    cors update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,8 @@ async function bootstrap() {
   // Enable CORS
   app.enableCors({
     origin: [
-      process.env.FRONTEND_URL || 'http://localhost:3000'
+      process.env.FRONTEND_URL || 'http://localhost:3000',
+      'https://slootea.com',
     ],
     credentials: true,
   });
