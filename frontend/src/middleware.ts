ea986c052bd64dcc91e2a1cc3722afb03e86be85
AlfaPigeon <oguzkaan.ozsoy@hotmail.com>
2026-03-11 16:48:38 +0300

    middleware fiix

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -75,7 +75,7 @@ export default clerkMiddleware(async (auth, request) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|mp4|webm|ogg|mp3|wav)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
